f562b56f1f5ade39f9ae6eea1ec95a32ecce8615
Zoé_Dazy <zoe.dazy+github@gmail.com~>
2023-10-31 10:26:17 +0100

    ajout fixtures admin

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,30 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(private UserPasswordHasherInterface $hasher) // injection du service de hachage de mot de passe avec l'interface PasswordHasher
+    {
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Faker\Factory::create('fr_FR');
+
+        $adminUser = new User();
+        $adminUser
+        ->setEmail('rh@frimousse.com')
+        ->setRoles(['ROLE_ADMIN'])
+        ->setPassword($this->hasher->hashPassword($adminUser, 'admninFrimousse23'))
+        ->setFirstname('Laetitia')
+        ->setLastname('Guillo');
+
+        $manager->persist($adminUser);
+
+
 
         $manager->flush();
     }
