79b205637f2b02215c9dc6715d3e3139c7cd391c
Zoé_Dazy <zoe.dazy@gmail.com>
2024-01-16 15:13:49 +0100

    Creation d'une Sélection OK

--- src/Controller/SelectionController.php
+++ src/Controller/SelectionController.php
@@ -2,20 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\Service;
 use App\Entity\User;
+use App\Repository\ArticleRepository;
 use App\Repository\SelectionRepository;
+use App\Repository\ServiceRepository;
 use App\Repository\UserRepository;
+use App\Service\CartManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class SelectionController extends AbstractController
 {
 
-    public function __construct(private SelectionRepository $selectionRepository, private SerializerInterface $serializer, private UserRepository $userRepository)
+    public function __construct(
+    private SelectionRepository $selectionRepository, 
+    private SerializerInterface $serializer, 
+    private UserRepository $userRepository,
+    private CartManager $cartManager,
+    private ArticleRepository $articleRepository,
+    private ServiceRepository $serviceRepository
+    )
     {}
 
     #[Route('/api/selections', name: 'app_selection', methods: ['GET'])] // IsGranted
@@ -31,4 +43,21 @@ class SelectionController extends AbstractController
         return $this->json($selections, 200, [], ['groups' => ['selection:read']]);
 
     }
+
+    #[Route('/api/selections', name: 'app_selection', methods: ['POST'])] // IsGranted
+    public function addItemToCart(Request $request, UserInterface $user): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $article = $this->articleRepository->findOneBy(['id' => $data['article']]);
+        $quantity = $data['quantity'];
+
+        $service = $this->serviceRepository->findOneBy(['id' => $data['service']]);
+
+        $newSelection = $this->cartManager->createNewSelection($article, $quantity, $user, $service);
+
+        return $this->json($newSelection, 201, [], ['groups' => ['selection:read']]);
+    }
+
+
 }
---
+++ src/Service/CartManager.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Article;
+use App\Entity\Selection;
+use App\Entity\Service;
+use App\Entity\User;
+use App\Repository\SelectionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class CartManager
+{
+    /**
+     * @var SelectionRepository
+     */
+    private $selectionRepository;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(
+        SelectionRepository $selectionRepository,
+        EntityManagerInterface $entityManager
+    ) {
+        $this->selectionRepository = $selectionRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * Create a new selection
+     *
+     * @param Article $article
+     * @param int $quantity
+     * @param User $user
+     * @param Service $service
+     * @return Selection
+     */
+    public function createNewSelection(Article $article, int $quantity, User $user, Service $service): Selection
+    {
+        $selectionPrice = (($article->getPrice() + $service->getPrice()) * $quantity);
+        $selection = new Selection();
+        $selection->setArticle($article);
+        $selection->setQuantity($quantity);
+        $selection->setUser($user);
+        $selection->setService($service);
+        $selection->setPrice($selectionPrice);
+
+        $this->entityManager->persist($selection);
+        $this->entityManager->flush();
+
+        return $selection;
+    }
+}
